8a235a3f0939e6076a929d27a2e9417eb8ce6525
siarhei_arkhutsik <sergey.arhutik@gmail.com>
2019-10-30 14:00:16 +0300

    add CRUD modules, routing and services

---
+++ src/app/order.ts.ts
@@ -0,0 +1,17 @@
+export class Order {
+  id: number;
+  orderDate: string;
+  orderStatus: string;
+  orderDeadline: string;
+  clientName: string;
+  clientPhone: string;
+  deviceDefect: string;
+  deviceType: string;
+  deviceIMEISN: string;
+  deviceBrand: string;
+  deviceModel: string;
+  deviceAppearance: string;
+  deviceComplectation: string;
+  devicePassword: string;
+  orderTotalCost: string;
+}
